77ba4b4d29be6aefaeb2a5c87ed06d220e006e56
gearuprepo <sb.desikan@gmail.com>
2020-05-27 22:24:41 +0530

    Updates

--- 03/MinHeap.py
+++ 03/MinHeap.py
@@ -17,15 +17,19 @@ class MinHeap:
         self.maxchild = 2 #2 For Binary representation
 
     def push(self,element):
+        print("-----end push--------"+str(element.key))
         self.container[self.current] = element
         #Construct Tree in parallel using LL
+        rtflag = False
         if self.current > 0: # For the element from index 1
             eletemploc = self.current + 1
             parentIndex = math.trunc(eletemploc/self.maxchild) - 1
             parentNode = self.container[parentIndex]
             element.parent = parentNode
             element.parentIndex = parentIndex
+
             if self.current%2 ==0:
+                rtflag = True
                 parentNode.right = element
                 parentNode.rightIndex = self.current
             else:
@@ -34,7 +38,6 @@ class MinHeap:
         pointer = self.current
         while self.container[pointer].parentIndex!=None and (self.container[pointer].value < self.container[pointer].parent.value):
             pointer = self.compareandswap(pointer)
-
         self.current += 1
     def pop(self):
         print("Pop")
@@ -45,16 +48,26 @@ class MinHeap:
         parentIndex = cachechild.parentIndex
 
         newChildBecomesParent = Node(cachechild.key,cachechild.value)
+        newParentBecomesChild = Node(cacheparent.key, cacheparent.value)
+
         newChildBecomesParent.parentIndex = cacheparent.parentIndex
         newChildBecomesParent.parent = cacheparent.parent
-        newChildBecomesParent.left = cacheparent.left
+        lft = cacheparent.left
+        rt = cacheparent.right
+        if elementindex%2 ==0:
+            rt = newParentBecomesChild
+        else:
+            lft = newParentBecomesChild
+        newChildBecomesParent.left = lft
         newChildBecomesParent.leftIndex = cacheparent.leftIndex
-        newChildBecomesParent.right = cacheparent.right
+        newChildBecomesParent.right = rt
         newChildBecomesParent.rightIndex = cacheparent.rightIndex
 
-        newParentBecomesChild = Node(cacheparent.key,cacheparent.value)
+
         newParentBecomesChild.parentIndex = cachechild.parentIndex
-        newParentBecomesChild.parent = cachechild.parent
+        newParentBecomesChild.parent = newChildBecomesParent
+        if elementindex % 2 == 0:
+            self.container[elementindex-1].parent = newChildBecomesParent
         newParentBecomesChild.left = cachechild.left
         newParentBecomesChild.leftIndex = cachechild.leftIndex
         newParentBecomesChild.right = cachechild.right
@@ -82,9 +95,11 @@ class MinHeap:
         return str(par)
 
 if __name__ == "__main__":
-    length = 15
-    #input = [3,55,20,1,5,88,20,21,45,1001,393,5,35,77,3,8,3,99,44,78,32,92,723,67]
-    input = [ele for ele in reversed(range(0,length))]
+    length = 4
+    input = [55,20,5,3,88,20,21,45,1001,393,3,35,77,8,99,44,2,78,32,5,92,723,6,67]
+    #input = [55, 88, 1001, 393, 77, 99,  78, 92, 723, 67]
+    #input = [ele for ele in reversed(range(0,length))]
+    #input = [55, 88, 21, 45]
     minheap = MinHeap(len(input))
     for i in input:
         node = Node("Key"+str(i),i)
