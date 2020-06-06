d25b92bf89caff263c13757f51d740aa9be13e09
gearuprepo <sdfa@cd.com>
2020-06-06 21:38:26 +0530

    04 Complete

--- 04/ActiveDirectory.py
+++ 04/ActiveDirectory.py
@@ -1,17 +1,18 @@
-# In Windows Active Directory, a group can consist of user(s) and group(s) themselves.
+# In Windows Active Directory, a group can consist of user(s) and group(s) themselves. 
 # We can construct this hierarchy as such.
 # Where User is represented by str representing their ids.
 class Group(object):
     def __init__(self, _name):
         self.name = _name
         self.groups = []
-        self.users = []
+        self.users = {}
 
     def add_group(self, group):
         self.groups.append(group)
 
     def add_user(self, user):
-        self.users.append(user)
+        #self.users.append(user)
+        self.users[user] = user
 
     def get_groups(self):
         return self.groups
@@ -22,25 +23,28 @@ class Group(object):
     def get_name(self):
         return self.name
 
-
-parent = Group("parent")
-child = Group("child")
-sub_child = Group("subchild")
-
-sub_child_user = "sub_child_user"
-sub_child.add_user(sub_child_user)
-
-child.add_group(sub_child)
-parent.add_group(child)
-
-# Write a function that provides an efficient look up of whether the user is in a group.
-
 def is_user_in_group(user, group):
     """
-    Return True if user is in the group, False otherwise.
+        Return True if user is in the group, False otherwise.
 
-    Args:
-      user(str): user name/id
-      group(class:Group): group to check user membership against
+        Args:
+        user(str): user name/id
+        group(class:Group): group to check user membership against
     """
-    return None
+    chi = group.users.get(user)
+    if chi != None:
+        return chi
+    else:
+        for gitem in group.groups:
+            return is_user_in_group(user,gitem)
+
+
+if __name__ == "__main__":
+    parent = Group("parent")
+    child = Group("child")
+    sub_child = Group("subchild")
+    sub_child_user = "sub_child_user"
+    sub_child.add_user(sub_child_user)
+    child.add_group(sub_child)
+    parent.add_group(child)
+    print(is_user_in_group("sub_child_user",parent))
