e45763e914df1947b3c2225780a3a13b48104475
gearuprepo <sdfa@cd.com>
2020-06-07 00:27:11 +0530

    updates to bc

--- 05/Blockchain.py
+++ 05/Blockchain.py
@@ -9,17 +9,37 @@ class Block:
 
       def calc_hash(self):
             sha = hashlib.sha256()
-            sha.update(self.data.encode('utf-8'))
+            tempstr = self.data + str(self.timestamp) + str(self.previous_hash)
+            sha.update(tempstr.encode('utf-8'))
             return sha.hexdigest()
 
 class BlockChain:
       def __init__(self):
             self.head = None
-
-      def push(node):
-            if self.head = None:
+            self.tail = None
+            self.hashmap = {}
+      
+      def get_tail(self):
+            return self.tail
+      
+      def push(self,node):
+            if self.head == None:
                   self.head = node
-
+                  self.tail = node
+                  self.hashmap[node.hash] = node
+            else:
+                  self.tail = node
+                  self.hashmap[node.hash] = node
+      
+      def __str__(self):
+            node = self.tail
+            outstr = ""
+            if node != None:
+                  outstr = outstr + (str(node.data) + " <- ")            
+                  while node.previous_hash != None:
+                        node = self.hashmap[node.previous_hash]
+                        outstr = outstr + (str(node.data) + " <- ")            
+            return outstr
 
 def const_data(data,prevhash):
       block = Block(time.gmtime(),data,prevhash)
@@ -28,5 +48,11 @@ def const_data(data,prevhash):
 
 if __name__ == "__main__":
      d1 = const_data("A",None) 
+     print(d1.hash)
+     d2 = const_data("B",d1.hash) 
+     print(d2.hash)
      bchain = BlockChain()
+     bchain.push(d1)
+     bchain.push(d2)
+     print(bchain)
 
