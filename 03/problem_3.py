710d871a8b02e450df861176c85eb79f7a163311
gearuprepo <sb.desikan@gmail.com>
2020-06-08 14:55:44 +0530

    Few updates

--- 03/problem_3.py
+++ 03/problem_3.py
@@ -131,7 +131,7 @@ class HuffCode:
 
 #a_great_sentence = "The bird is the word"
 #a_great_sentence = "Testing Huffman's coding"
-a_great_sentence = ""
+a_great_sentence = "T"
 
 print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
 print("The content of the data is: {}\n".format(a_great_sentence))
