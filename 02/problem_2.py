9dc9bdbdf2e9450f6309d099495f5e6656a0ba7a
gearuprepo <sb.desikan@gmail.com>
2020-06-08 11:51:18 +0530

    Fixing 02

--- 02/problem_2.py
+++ 02/problem_2.py
@@ -2,25 +2,29 @@
 import os
 base_path = os.path.dirname(os.path.realpath(__file__))
 path = base_path+"/testdir"
-
-def loopdir( path):
+#Fix 3: Do not hard code suffix.
+suff = ".c"
+def loopdir( path,suffix):
     ls_files = []
-    for f in os.listdir(path):
-        localpath = path + "/" + f
-        if os.path.isdir(localpath):
-            ls_files.append(localpath)
-            ret_ls = loopdir(localpath)
-            if ret_ls.__len__() ==0:
-                ls_files.remove(localpath)
+    #Fix 1: Check for dir
+    if os.path.isdir(path):
+        for f in os.listdir(path):
+            #Fix 2: For cross platform compatibility
+            localpath = os.path.join(path,f)
+            if os.path.isdir(localpath):
+                ls_files.append(localpath)
+                ret_ls = loopdir(localpath,suffix)
+                if ret_ls.__len__() ==0:
+                    ls_files.remove(localpath)
+                else:
+                    for r in ret_ls:
+                        ls_files.append(r)
             else:
-                for r in ret_ls:
-                    ls_files.append(r)
-        else:
-            if str(path +"/"+f).endswith(".c"):
-                ls_files.append(path +"/"+f)
+                if str(path +"/"+f).endswith(suffix):
+                    ls_files.append(path +"/"+f)
     return ls_files
 
-for p in loopdir(path):
+for p in loopdir(path,suff):
     print(str(p).replace(base_path,"."))
 
 
