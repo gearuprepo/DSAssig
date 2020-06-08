ea1c79598a4893009c72496b9c355d8a38a36c22
gearuprepo <sb.desikan@gmail.com>
2020-06-08 14:37:16 +0530

    Fix 01

--- 01/problem_1.py
+++ 01/problem_1.py
@@ -1,96 +1,29 @@
 #LRU cache
 
-class LinkedListNode:
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
-
+import collections
+# Fixed based on the comment to use OrderedDict
 class LRU_Cache:
+    def __init__(self, initial_size):
+        self.num_entries = initial_size
+        self.bucket_cache = collections.OrderedDict()
 
-    def __init__(self, initial_size=15):
-        self.bucket_array = [None for _ in range(initial_size)]
-        self.num_entries = 0
-        self.master_q = []
-
-    def set(self, key, value):
-        # Implement LRU Start, Incase of cache full, delete LRU O(n) = n
-        if self.num_entries > len(self.bucket_array) - 1:
-            to_be_deleted = self.master_q[0]
-            self.delete(to_be_deleted)
-        # Implement LRU End
-
-        bucket_index = self.get_bucket_index(key)
-        new_node = LinkedListNode(key, value)
-        head = self.bucket_array[bucket_index]
-
-        # check if key is already present in the map, and update it's value
-        while head is not None:
-            if head.key == key:
-                head.value = value
-                return
-            head = head.next
-
-        # key not found in the chain --> create a new entry and place it at the head of the chain
-        head = self.bucket_array[bucket_index]
-        new_node.next = head
-        self.bucket_array[bucket_index] = new_node
-        self.num_entries += 1
-
-        # Add to master Queue for implementing LRU
-        self.master_q.append(key)
-
+    #O(n) = Constant
     def get(self, key):
-        bucket_index = self.get_hash_code(key)
-        head = self.bucket_array[bucket_index]
-        while head is not None:
-            if head.key == key:
-                # LRU Implementation Start
-                temp_q = []
-                temp = None
-                for ele in self.master_q:
-                    if ele == key:
-                        temp = ele
-                    else:
-                        temp_q.append(ele)
-                temp_q.append(temp)
-                self.master_q = temp_q
-                # LRU Implementation End
-                return head.value
-            head = head.next
-        return -1
-
-    def get_bucket_index(self, key):
-        bucket_index = self.get_hash_code(key)
-        return bucket_index
-
-    def get_hash_code(self, key):
-        num_buckets = len(self.bucket_array)
-        hash_code = key % num_buckets  # Simple hash code with compression
-        return hash_code
-
-    def size(self):
-        return self.num_entries
-
-    def delete(self, key):
-        bucket_index = self.get_bucket_index(key)
-        head = self.bucket_array[bucket_index]
-
-        previous = None
-        while head is not None:
-            if head.key == key:
-                if previous is None:
-                    self.bucket_array[bucket_index] = head.next
-                else:
-                    previous.next = head.next
-                self.num_entries -= 1
-                return
-            else:
-                previous = head
-                head = head.next
-
+        try:
+            value = self.bucket_cache.pop(key)
+            self.bucket_cache[key] = value
+            return value
+        except KeyError:
+            return -1
+    #O(n) = Constant
+    def set(self, key, value):
+        try:
+            self.bucket_cache.pop(key)
+        except KeyError:
+            if len(self.bucket_cache) >= self.num_entries:
+                # LIFO order if last is true or FIFO order if false.
+                self.bucket_cache.popitem(last=False)
+        self.bucket_cache[key] = value
 # Testing
 
 our_cache = LRU_Cache(5)
